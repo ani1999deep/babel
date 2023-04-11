be37d7fe2ea1c2525ab856f4954e35f136e75264
ani1999deep <anideepbhowmick1@gmail.com>
2023-04-11 14:52:49 +0530

    first commit

---
+++ dist/sample.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let sum = (a, b) => {
+  return a, b;
+};
+console.log("Sum is of two values", sum(10, 20));
---
+++ src/sample.js
@@ -0,0 +1,6 @@
+let sum=(a,b)=>{
+
+      return a,b;
+
+}
+console.log("Sum is of two values",sum(10,20))
